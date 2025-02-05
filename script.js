4e822eee0f3f0b615732aad8b7d8d86c67470191
avi-codesmith <avi.codesmith@gmail.com>
2025-02-05 19:57:05 +0530

    JS-pointer-effect-1

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+document.addEventListener("mousemove", (e) => {
+  let trail = document.createElement("div");
+  trail.className = "trail";
+  trail.innerText = "⚪";
+  document.body.appendChild(trail);
+
+  trail.style.left = `${e.pageX}px`;
+  trail.style.top = `${e.pageY}px`;
+
+  setTimeout(() => {
+    trail.remove();
+  }, 300);
+});
